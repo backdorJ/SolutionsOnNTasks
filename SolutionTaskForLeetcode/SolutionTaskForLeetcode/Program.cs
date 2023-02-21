4f51d0071d2ed8a7c686d58cef68087e3a012393
Nabiullin Damir <cdbacjdorz@yandex.ru>
2023-02-21 19:37:47 +0300

    Add Solution

---
+++ SolutionTaskForLeetcode/SolutionTaskForLeetcode/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        ReverseString(new char[] { 'h', 'e', 'l', 'l', 'o' });
+    }
+
+    public static void ReverseString(char[] s)
+    {
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            var temp = s[i];
+            s[i] = s[s.Length - 1 - i];
+            s[s.Length - 1 - i] = temp;
+        }
+    }
+}
