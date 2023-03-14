59ce677667ab0bd71560eeee335aff55407556a2
Nabiullin Damir <cdbacjdorz@yandex.ru>
2023-03-14 23:31:27 +0300

    Add Solution For Task

---
+++ CollectionClassDelegate/CollectionClassDelegate/CollectionClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace CollectionClassDelegate
+{
+    internal class CollectionClass<T>
+    {
+        private new List<T> _collection;
+        public CollectionClass(List<T> collection)
+        {
+            _collection = collection;
+        }
+
+        public int Count(Func<T, bool> predicate) 
+        {
+            var count = 0;
+            foreach (var item in _collection)
+            {
+                if (predicate(item))
+                    count++;
+            }
+
+            return count;
+        }
+
+        public T1 Aggreagete<T1>(Func<T1,T,T1> function)
+        {
+            var temp = default(T1);
+            foreach (var item in _collection) 
+                temp = function(temp,item);
+
+            return temp;
+        }
+    }
+}
---
+++ CollectionClassDelegate/CollectionClassDelegate/Program.cs
@@ -0,0 +1,52 @@
+﻿using CollectionClassDelegate;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        FithMethod();
+    }
+
+    public static void FirstMethod()
+    {
+        var list = new List<int> { 1, 2, 3, 4 };
+        var collectionClass = new CollectionClass<int>(list);
+        var result = collectionClass.Aggreagete<int>((x, y) => x + y);
+        Console.WriteLine(result);
+    }
+
+    public static void SecondMethod()
+    {
+        var list = new List<int> { 1, 2, 3, 4 };
+        var collectionClass = new CollectionClass<int>(list);
+        var result = collectionClass.Aggreagete<int>((x, y) => x - y);
+        Console.WriteLine(result);
+    }
+
+    public static void ThirdMethod()
+    {
+        var list = new List<string> { "1", "2", "5", "23", "24" };
+        var collectionClass = new CollectionClass<string>(list);
+        var result = collectionClass.Aggreagete<string>((x, y) => x + y);
+        var res = collectionClass.Count(x => x.StartsWith("2"));
+        Console.WriteLine(result);
+        Console.WriteLine(res);
+    }
+
+    public static void FourdMethod()
+    {
+        var list = new List<string> { "1", "2", "5", "23", "24" };
+        var collectionClass = new CollectionClass<string>(list);
+        var result = collectionClass.Aggreagete<string>((x, y) => $"{x} : {y}");
+        Console.WriteLine(result);
+    }
+
+    public static void FithMethod()
+    {
+        var list = new List<int> { 1, 2, 3, 4 };
+        var collectionClass = new CollectionClass<int>(list);
+        var result = collectionClass.Aggreagete<int>((x, y) => x * 2 + y * 2);
+        Console.WriteLine(result);
+    }
+}
